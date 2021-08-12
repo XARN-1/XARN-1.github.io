7528ce2255f6edfc67c3a06373f32bc5e5580b55
rendi-18 <rendidwipratama99@gmail.com>
2021-08-12 23:17:29 +0700

    nambah

---
+++ Konstruksi/Kebutuhan/script.js
@@ -0,0 +1,21 @@
+$(".desain").click(function(){
+    $('.check1').toggleClass('aktif');
+    $('.konten').toggleClass('aktif1');
+});
+
+$(".rab").click(function(){
+    $('.check2').toggleClass('aktif');
+    $('.konten').toggleClass('aktif2');
+});
+
+$(".tdk").click(function(){
+    $('.check3').toggleClass('aktif');
+    $('.konten').toggleClass('aktif3');
+});
+
+
+
+
+
+
+
---
+++ Renovasi/script.js
@@ -0,0 +1,33 @@
+$(".kotak1").click(function(){
+    $('.kotak1').addClass('click');
+    $('.kotak2').removeClass('click');
+    $('.kotak3').removeClass('click');
+    $('.kotak4').removeClass('click');
+    $('.formulir').addClass('mundur');
+});
+
+$(".kotak2").click(function(){
+    $('.kotak2').addClass('click');
+    $('.kotak1').removeClass('click');
+    $('.kotak3').removeClass('click');
+    $('.kotak4').removeClass('click');
+    $('.formulir').addClass('mundur');
+});
+
+$(".kotak3").click(function(){
+    $('.kotak3').addClass('click');
+    $('.kotak1').removeClass('click');
+    $('.kotak2').removeClass('click');
+    $('.kotak4').removeClass('click');
+    $('.formulir').addClass('mundur');
+});
+
+$(".kotak4").click(function(){
+    $('.kotak4').addClass('click');
+    $('.kotak1').removeClass('click');
+    $('.kotak2').removeClass('click');
+    $('.kotak3').removeClass('click');
+    $('.formulir').addClass('mundur');
+});
+
+
